75efba386f25cbc42994449bc3f88bdf09705154
Developer <mishant@gmail.com>
2015-04-10 04:39:15 +0530

    contact us mail implementation on index.html page

--- js/landing.js
+++ js/landing.js
@@ -1,39 +1,75 @@
-jQuery.extend( jQuery.easing,
-{
+jQuery.extend(jQuery.easing, {
     def: 'easeOutQuad',
-    easeInOutExpo: function (x, t, b, c, d) {
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    easeInOutExpo: function(x, t, b, c, d) {
+        if (t == 0) return b;
+        if (t == d) return b + c;
+        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
     }
 });
 
-!function ($) {
+! function($) {
 
-  $(function(){
+    $(function() {
 
-    $('[data-ride="animated"]').addClass('invisible');
-    $('[data-ride="animated"]').appear();
-	$('[data-ride="animated"]').on('appear', function() {
-        var $el = $(this), $ani = ($el.data('animation') || 'fadeIn'), $delay;
-        if ( !$el.hasClass('animated') ) {
-        	$delay = $el.data('delay') || 0;
-            setTimeout(function(){
-                $el.removeClass('invisible').addClass( $ani + " animated" );
-            }, $delay);
-        }
-    });
-    
-    $(document).on('click.app','[data-ride="scroll"]',function (e) {
-        e.preventDefault();
-        var $target = this.hash;
-        $('html, body').stop().animate({
-            'scrollTop': $($target).offset().top - 80
-        }, 1000, 'easeInOutExpo', function () {
-            window.location.hash = $target;
+        $('[data-ride="animated"]').addClass('invisible');
+        $('[data-ride="animated"]').appear();
+        $('[data-ride="animated"]').on('appear', function() {
+            var $el = $(this),
+                $ani = ($el.data('animation') || 'fadeIn'),
+                $delay;
+            if (!$el.hasClass('animated')) {
+                $delay = $el.data('delay') || 0;
+                setTimeout(function() {
+                    $el.removeClass('invisible').addClass($ani + " animated");
+                }, $delay);
+            }
+        });
+
+        $(document).on('click.app', '[data-ride="scroll"]', function(e) {
+            e.preventDefault();
+            var $target = this.hash;
+            $('html, body').stop().animate({
+                'scrollTop': $($target).offset().top - 80
+            }, 1000, 'easeInOutExpo', function() {
+                window.location.hash = $target;
+            });
+        });
+
+        jQuery('#contactUs').submit(function(event) {
+            jQuery('#errorMessage').hide();
+            event.preventDefault();
+            var action = "http://api.nextshopper.com/ws/message/email-us";
+            jQuery('#submit')
+                .after('<img src="img/ajax-loader.gif" class="loader pull-right" />')
+                .attr('disabled', 'disabled');
+            var data = {
+                'name': jQuery('#name').val(),
+                'from': jQuery('#email').val(),
+                'subject': jQuery('#subject').val(),
+                'content': jQuery('#message').val()
+            };
+            jQuery.ajax({
+                url: action,
+                method: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function(data) {
+                    if (data.msg && data.msg === 'ok') {
+                        jQuery('#successMessage').slideToggle('fast');
+                        jQuery('#successMessage').html('Your mail has been sent! We will contact you shortly.<br>Thank you');
+                        jQuery('#contactUs').slideUp('fast');
+                    } else {
+                        jQuery('#errorMessage').slideToggle('fast');
+                        jQuery('#errorMessage').html('Sorry! We are facing some technical problem right now, please try again later.');
+                        jQuery('#submit').removeAttr('disabled');
+                    }
+                    jQuery('#contactUs img.loader').fadeOut('fast', function() {
+                        jQuery(this).remove()
+                    });
+                }
+            });
+            return false;
         });
     });
-    
-  });
 }(window.jQuery);
