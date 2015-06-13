8c26099d061cb9858636adbcddf38ad35f1e68b0
Developer <mishant@gmail.com>
2015-06-14 01:17:52 +0530

    footer updated

--- l10n/en.js
+++ l10n/en.js
@@ -14,12 +14,15 @@
     "footer": {
         "ABOUT_US":"About us",
         "FAQ":"FAQ",
+        "MERCHANT_FAQ":"Merchant FAQ",
+        "TERM_OF_SERVICE":"Term Of Services",
         "BUYER_PROTECTION_PROGRAM":"Buyer Protection Program",
         "RETURN_POLICY":"Return &amp; Refund Policy",
         "SELL_ON_NEXTSHOPPER":"Sell on Nextshopper?",
         "TERM_OF_USE":"Term of Use",
         "PRIVACY_POLICY":"Privacy Policy",
+        "MERCHANT_POLICY":"Merchant Policy",
         "COPYRIGHT":"Nextshopper Inc. &copy; 2015",
-        "ABOUT_US":"About us"
+        "DOWNLOAD_APP":"download App?"
     }
 }
